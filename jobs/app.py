4c743d4936e263d8540ea3cec10ba0b1be269b2f
Chris Martin <chris.martin2@coop.co.uk>
2021-09-03 17:04:05 +0100

    sql connect

--- jobs/app.py
+++ jobs/app.py
@@ -1,9 +1,36 @@
-from flask import Flask
-from flask import render_template
+#from sqlite3.dbapi2 import connect
+import sqlite3
+from flask import Flask, render_template, globals as g
+
+
+PATH = "db/jobs.sqlite"
 
 app = Flask(__name__)
+
+def open_connection():
+    connection = getattr(g, '_connection', None)
+    if connection is None:
+     connection = g._connection = sqlite3.connect(PATH)
+    connection.row_factory = sqlite3.Row
+    return connection
+
+def execute_sql(sql, values=(), commit=False, single=False):
+    connection = open_connection()
+    cursor = connection.execute(sql, values)
+    if commit == True:
+     results = connection.commit()
+    else:
+     results = cursor.fetchone() if single else cursor.fetchall()
+    connection.close()
+    return results
 
 @app.route("/")
 @app.route("/jobs")
 def jobs():
     return render_template('index.html')
+
+@app.teardown_appcontext
+def close_connection(exception):
+    connection = getattr(g, '_connection', None)
+    if connection is not None:
+        connection.close()
